5140cb6f1d8d92e46e0ab1ffd3fd1cfb87e7fb1d
TassioSales <74218122+TassioSales@users.noreply.github.com>
2024-10-23 19:16:50 -0300

    Update ifElse.java

--- aula14/src/ifElse.java
+++ aula14/src/ifElse.java
@@ -11,5 +11,24 @@ public class ifElse {
         } else {
             System.out.println("E menor de idade");
         }
+        //barato <= 10
+        //normal 10 < valor < 15 - pede desconto
+        //caro <= 15  valor 17 - pesquisar mais
+        //muito caro >= 17  - muito caro sair fora
+
+        System.out.println("Digite o valor do produto: ");
+        double valor = scan.nextDouble();
+        if (valor <= 10) {
+            System.out.println("Barato");
+        } else if (valor > 10 && valor < 15) {
+            System.out.println("Normal");
+            System.out.println("Voce pode pedir desconto");
+        } else if (valor >= 15 && valor < 17) {
+            System.out.println("Caro");
+            System.out.println("Pesquisar mais");
+        } else {
+            System.out.println("Muito caro");
+            System.out.println("Sair fora");
+        }
     }
 }
