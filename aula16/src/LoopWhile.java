1f7c582577786aca0b51824dd186d9f3f7258969
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-04-01 17:13:00 -0300

    Create LoopWhile.java

---
+++ aula16/src/LoopWhile.java
@@ -0,0 +1,15 @@
+
+
+public class LoopWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        int max = 10;
+        System.out.println("Contando até " + max);
+        while (i <= max) {
+            System.out.println("O valor de i e: " + i);
+            i++;
+        }
+        System.out.println("O valor final de i e: " + i);
+        System.out.println("Fim");
+    }
+}
